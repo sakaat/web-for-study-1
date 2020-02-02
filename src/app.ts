6dccf710ff42b7500deba507eaa35a4a36175557
sakata.tomoyuki <a1819ts@aiit.ac.jp>
2020-02-02 10:48:39 +0900

    Slack連携確認

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,15 @@ app.use(express.urlencoded({ extended: true }));
 
 import session = require("express-session");
 
+import AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_DEFAULT_REGION,
+});
+
+import request = require("request");
+
 app.use(
     session({
         secret: "secret",
@@ -37,15 +46,8 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-    const AWS = require("aws-sdk");
-    var docClient = new AWS.DynamoDB.DocumentClient({
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: process.env.AWS_DEFAULT_REGION,
-    });
-    const tableName = "web-for-study-1-users";
     const params = {
-        TableName: tableName,
+        TableName: "web-for-study-1-users",
         Key: {
             "username": req.body.username,
         },
@@ -69,8 +71,68 @@ app.get("/register", async (req, res) => {
     res.render("./register.ejs");
 });
 
-app.post("/register", (_req, res) => {
-    res.redirect("/");
+app.post("/register", async (req, res) => {
+    const params = {
+        TableName: "web-for-study-1-directory",
+        Key: {
+            "userid": req.body.userid,
+        },
+    };
+    const data = await docClient.get(params).promise();
+    console.log(data);
+
+    if (data.Item.username === req.body.username1) {
+        console.log("成功");
+        const payload = `payload=
+            {
+                "text": "Slack Message Sample Text",
+                "attachments": [
+                    {
+                        "fallback": "fallback string",
+                        "title": "${req.body.username1} が ${req.body.username2} に承認を求めています",
+                        "callback_id": "callback_id value",
+                        "color": "#FF0000",
+                        "attachment_type": "default",
+                        "actions": [
+                            {
+                                "name": "btn1Name",
+                                "text": "APPROVE",
+                                "type": "button",
+                                "style": "default",
+                                "value": "approve"
+                            },
+                            {
+                                "name": "btn2Name",
+                                "text": "REJECT",
+                                "type": "button",
+                                "style": "default",
+                                "value": "reject"
+                            }
+                        ]
+                    }
+                ],
+                "response_type": "ephemeral",
+                "replace_original": true,
+                "delete_original": true
+            }`;
+        const options = {
+            url: process.env.SLACK_URL,
+            headers: { "cache-control": "no-cache" },
+            form: payload,
+            json: true,
+        };
+
+        request.post(options, (error) => {
+            if (error) {
+                throw new Error(error);
+            }
+        });
+
+        res.redirect("/");
+    } else {
+        console.log("失敗");
+        res.render("./login.ejs");
+    }
 });
 
 app.post("/slack", (req, res) => {
